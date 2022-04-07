1bda300c8d0d0e49125c972b908070b918e34123
Dusan Kokotovic <kokotovic999@gmail.com>
2022-04-07 13:04:16 +0200

    Gotov front

--- Projekat/src/app/PotrosacKomponente/potrosac-home/potrosac-home.component.ts
+++ Projekat/src/app/PotrosacKomponente/potrosac-home/potrosac-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Porudzbina } from 'src/app/models/porudzbina.model';
 
 @Component({
   selector: 'app-potrosac-home',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./potrosac-home.component.css']
 })
 export class PotrosacHomeComponent implements OnInit {
+  
+  Lista: Porudzbina[] = [
+    {vrijeme : new Date(new Date().getTime() + 1*60000), dostavljac:"", narucilac:"dulek99", hrana:"Aloooo", kolicina:2,cijena:500,komentar:"kada dodje kurir da kaze Pomaze Bog",adresa:"Filipa Filipovica",status:"Dostavlja se"},
+    {vrijeme : new Date(new Date().getTime() + 2*60000),dostavljac:"", narucilac:"dulek99", hrana:"Capricosa", kolicina:2,cijena:500,komentar:"kada dodje kurir da kaze Pomaze Bog",adresa:"Filipa Filipovica",status:"Dostavlja se"},    
+    {vrijeme : new Date(new Date().getTime() - 5*60000),dostavljac:"", narucilac:"dulek99", hrana:"Capricosa", kolicina:2,cijena:500,komentar:"kada dodje kurir da kaze Pomaze Bog",adresa:"Filipa Filipovica",status:"Dostavlja se"},    
+    {vrijeme : new Date(new Date().getTime() - 5*60000),dostavljac:"", narucilac:"dulek99", hrana:"Capricosa", kolicina:2,cijena:500,komentar:"kada dodje kurir da kaze Pomaze Bog",adresa:"Filipa Filipovica",status:"Dostavlja se"},
+    {vrijeme : new Date,dostavljac:"", narucilac:"dulek99", hrana:"Capricosa", kolicina:2,cijena:500,komentar:"kada dodje kurir da kaze Pomaze Bog",adresa:"Filipa Filipovica",status:"Izvrseno"},
+    {vrijeme : new Date,dostavljac:"", narucilac:"dulek99", hrana:"Capricosa", kolicina:2,cijena:500,komentar:"kada dodje kurir da kaze Pomaze Bog",adresa:"Filipa Filipovica",status:"Izvrseno"}   
+   ];
+   prikaz: Porudzbina[]= new Array()
 
   constructor() { }
 
   ngOnInit(): void {
-  }
+
+    this.Lista.forEach(element => {
+      if(element.vrijeme < new Date()){
+        element.status = "Izvrseno";
+      }
+      if(element.status === "Dostavlja se"){
+        this.prikaz.push(element);
+      }
+      
+    });
+    console.log(this.Lista);
+    }
+  
 
 }
--- Projekat/src/app/brojcanik/brojcanik.component.ts
+++ Projekat/src/app/brojcanik/brojcanik.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Porudzbina } from '../models/porudzbina.model';
 import { PorudzbinaPrikaz } from '../models/porudzbinaprikaz.model';
 
@@ -9,60 +9,26 @@ import { PorudzbinaPrikaz } from '../models/porudzbinaprikaz.model';
 })
 export class BrojcanikComponent implements OnInit {
 
-  role : string = "Dostavljac";
-
-  Lista: Porudzbina[] = [
-    {vrijeme : new Date(new Date().getTime() + 1*60000), dostavljac:"", narucilac:"dulek99", hrana:"Aloooo", kolicina:2,cijena:500,komentar:"kada dodje kurir da kaze Pomaze Bog",adresa:"Filipa Filipovica",status:"Dostavlja se"},
-    {vrijeme : new Date,dostavljac:"", narucilac:"dulek99", hrana:"Capricosa", kolicina:2,cijena:500,komentar:"kada dodje kurir da kaze Pomaze Bog",adresa:"Filipa Filipovica",status:"Izvrseno"},    
-    {vrijeme : new Date,dostavljac:"", narucilac:"dulek99", hrana:"Capricosa", kolicina:2,cijena:500,komentar:"kada dodje kurir da kaze Pomaze Bog",adresa:"Filipa Filipovica",status:"Izvrseno"},    
-    {vrijeme : new Date,dostavljac:"", narucilac:"dulek99", hrana:"Capricosa", kolicina:2,cijena:500,komentar:"kada dodje kurir da kaze Pomaze Bog",adresa:"Filipa Filipovica",status:"Aktivna"},
-    {vrijeme : new Date,dostavljac:"", narucilac:"dulek99", hrana:"Capricosa", kolicina:2,cijena:500,komentar:"kada dodje kurir da kaze Pomaze Bog",adresa:"Filipa Filipovica",status:"Izvrseno"},
-    {vrijeme : new Date,dostavljac:"", narucilac:"dulek99", hrana:"Capricosa", kolicina:2,cijena:500,komentar:"kada dodje kurir da kaze Pomaze Bog",adresa:"Filipa Filipovica",status:"Izvrseno"}
-    ];
-
-  item: PorudzbinaPrikaz = new PorudzbinaPrikaz;
-  
-  constructor() {
-    this.Lista.forEach(element => {
-      if(element.status == "Dostavlja se"){
-        this.item.hrana = element.hrana;
-        this.item.kolicina = element.kolicina;
-        this.item.adresa = element.adresa;
-        this.item.cijena = element.cijena;
-        this.item.komentar = element.komentar;
-        this.item.status = element.status;
-        //this.drugi = element;
-      }
-    });
-  }
-
-  ngOnInit(): void {}
-  countDownDate = new Date(new Date().getTime() + 1*60000).getTime();
+  @Input() childMessage: Date = new Date();
   demo:any;
-  
-  x = setInterval(() =>{
-    var now = new Date().getTime();
-    var distance = this.countDownDate - now;
-    var minutes = Math.floor((distance % (1000*60*60))/ (1000*60));
-    var seconds = Math.floor((distance % (1000*60)) / 1000);
-    this.demo = minutes + " minuta " + seconds + " sekundi";
-    if(minutes < 0 || seconds <0){
-      minutes = 0;
-      seconds = 0;
-    }
-    if(minutes === 0 && seconds === 0){
-      clearInterval(this.x);
-      this.item.status = "Izvrseno"
-    }
-    
-  })
-
-  
-  DodavanjeTajmera(vrijeme:Date){
-
+  constructor() {}
+  ngOnInit(): void {
+    var countDownDate  = this.childMessage.getTime();
+    var x = setInterval(() =>{
+      var now = new Date().getTime();
+      var distance = countDownDate - now;
+      var minutes = Math.floor((distance % (1000*60*60))/ (1000*60));
+      var seconds = Math.floor((distance % (1000*60)) / 1000);
+      this.demo = minutes + " minuta " + seconds + " sekundi";
+      if(minutes < 0 || seconds <0){
+        minutes = 0;
+        seconds = 0;
+      }
+      if(minutes === 0 && seconds === 0){
+        clearInterval(x);
+      }       
+      }) 
   }
-
-
           
     
 }
